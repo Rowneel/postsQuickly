526473faa11e62f97fbb4cbe1e83f38287cb8a88
[pali-s] <[palishma077@academiacollege.edu.np]>
2024-10-09 12:59:31 +0545

    features-component

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import Features from '../components/Features'
 
 function LandingPage() {
   return (
-    <div>LandingPage</div>
+    
+    <div>
+      <Features/>
+    </div>
+    
   )
 }
 
